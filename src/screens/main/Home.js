209074d82a9fdea41e2809e3ad8b41c98cede4bf
kashershahzad <kashershahzadprogramming@gmail.com>
2025-05-10 13:38:13 +0500

    added searchbar file

--- src/screens/main/Home.js
+++ src/screens/main/Home.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import Topbar from '../../../components/main/Topbar'
+import Searchbar from '../../../components/main/Searchbar'
 
 const Home = () => {
 
     return (
         <View style={styles.container}>
             <Topbar />
+            <Searchbar />
         </View>
     );
 }
